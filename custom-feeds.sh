4353f8c05811868bf47201bb4040ae7e20afb746
nwntech <i@nwn.moe>
2022-03-05 16:19:25 +0800

    Create Configurations

--- custom-feeds.sh
+++ custom-feeds.sh
@@ -16,3 +16,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
+
+# Add extra packages
+git clone https://github.com/jerrykuku/luci-theme-argon package/extra-pkgs/luci-them-argon
